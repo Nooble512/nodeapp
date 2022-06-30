707161b4000f610a37ee9f1f92fc822777ec878b
Nooble512 <rjorgeliy@gmail.com>
2022-06-29 21:17:03 -0500

    Se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 8080
+
+app.get('/integrantes', function(req, res) {
+    res.json({
+        Integrantes: [
+            {
+                Nombre: "Bryan Zunun Marto",
+                Matricula:"183388",
+                Grupo:"A"
+            },
+            {
+                Nombre: "Jorge Roberto Aguirre Liy",
+                Matricula:"183386",
+                Grupo:"B"
+            }
+        ]
+    });
+});
+
+app.get('/', async(_req, res, _next) => {
+    try {
+        res.send("All ok");
+    } catch (error) {
+        healtcheck.message = error;
+        res.status(503).send();
+    }
+});
+
+app.listen(port, function(req, res) {
+    console.log("Server is running at port ",port);
+});
